88fceb1c35e48922ea93f036cd98d03ff2fe5513
Forzenook <antonlukash05@gmail.com>
2022-11-25 22:17:40 +0200

    Init commit. Worked application

---
+++ LABA3.cpp
@@ -0,0 +1,118 @@
+﻿#include <fstream>
+#include <locale>
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void checkValidInput()
+{
+    if (cin.fail())
+    {
+        throw "Incorrect input";
+    }
+}
+void checkValidParams(int n)
+{
+    if (n < 1)
+    {
+        throw "Input correct data";
+    }
+
+}
+int calculate(int n, int x)
+{
+
+
+    if (x <= 0)
+    {
+        double y = 1;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            y *= (i * i + i);
+        }
+
+        return y;
+    }
+    else
+    {
+        double y = 0;
+        for (int i = 0; i <= n - 1; i++)
+            for (int j = 0; j <= n - 1; j++)
+                if (j + i != 0)
+                {
+                    y += x / (i + j);
+                }
+        return y;
+
+
+
+    }
+
+
+}
+
+
+
+
+int main()
+{
+
+    ofstream fout("D:\\test.txt");
+
+    int a, n, b, h;
+    int value;
+
+    try {
+
+        cout << "Input n>=1, n=";
+        cin >> n;
+        checkValidInput();
+        checkValidParams(n);
+
+        cout << "Input a=";
+        cin >> a;
+        double x = a;
+        checkValidInput();
+        cout << "Input b =";
+        cin >> b;
+        cout << "Input h=";
+        cin >> h;
+
+
+        cout << "Enter 1 if you want to save the results to a file" << endl;
+        cin >> value;
+        while (x <= b)
+        {
+            cout << "x=" << x << ";" << "y=" << calculate(n, x) << endl;
+
+
+            if (value == 1)
+            {
+                fout << "x=" << x << endl;
+                fout << "y=" << calculate(n, x) << endl;
+
+            }
+            x = x + h;
+        }
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+        return-1;
+    }
+    catch (...)
+    {
+        cout << "Unknown error" << endl;
+        return -2;
+    }
+
+}
+
+
+
+    
+    
+
+
---
+++ UnitTest2.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\ЛАБЫ VS\ЛАБА3 ИПЗ\ЛАБА3 ИПЗ\ЛАБА3 ИПЗ.cpp"
+#include <expected>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+    TEST_CLASS(CalculateTest)
+    {
+    public:
+
+        TEST_METHOD(Calculate_get4and1and1and1_2returned)
+        {
+
+            int n = 4;
+            int x = 1;
+            int b = 1;
+            int h = 1;
+
+            int actual = calculate(n, x);
+            int expected = 2;
+            Assert::AreEqual(expected, actual);
+        }
+    public:
+
+        TEST_METHOD(Calculate_get8and6and6and1_47returned)
+        {
+
+            int n = 8;
+            int x = 6;
+            int b = 6;
+            int h = 1;
+
+            int actual = calculate(n, x);
+            int expected = 47;
+            Assert::AreEqual(expected, actual);
+        }
+    };
+    TEST_CLASS(checkValidParamsTests)
+    {
+    public:
+
+        TEST_METHOD(checkValidParams_get8_exceptionNotTrown)
+        {
+
+            int n = 8;
+            try
+            {
+                checkValidParams(n);
+                Assert::IsTrue(true);
+            }
+            catch (...)
+            {
+                Assert::Fail();
+            }
+
+        }
+    public:
+
+        TEST_METHOD(checkValidParams_get0_exceptionNotTrown)
+        {
+
+            int n = 0;
+            try
+            {
+                checkValidParams(n);
+                Assert::Fail();
+            }
+            catch (...)
+            {
+                Assert::IsTrue(true);
+            }
+
+        }
+    };
+}
